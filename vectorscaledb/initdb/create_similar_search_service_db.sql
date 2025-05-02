1a2082d587827fa19c07da332f3d859de6c3fa63
pawel-laptop <p.blaszczy@gmail.com>
2025-05-02 08:46:12 +0200

    add db

---
+++ vectorscaledb/initdb/create_similar_search_service_db.sql
@@ -0,0 +1,7 @@
+-- Create the database for the similarity search service
+CREATE DATABASE images;
+
+-- Connect to the database (not needed)
+
+-- Enable vectorscale extension - crashing, have to do it manually
+-- CREATE EXTENSION IF NOT EXISTS vectorscale CASCADE;
